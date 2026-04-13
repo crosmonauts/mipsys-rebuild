43340a8fbe236713983de48795eda2777f1ed80f
crosmonauts <nandaadam25@gmail.com>
2026-04-13 20:10:18 +0700

    remove redundant mipsys-rebuild folder

--- mipsys-rebuild/mipsys-backend/src/scraper/scraper.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'; // <--- INI KTM YANG HILANG!
-import { ScraperService } from './scraper.service';
-
-@Module({
-  providers: [ScraperService],
-  exports: [ScraperService], // Agar bisa dipakai modul lain seperti Service Request
-})
-export class ScraperModule {}
